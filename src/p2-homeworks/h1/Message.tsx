8e576d225db2a032a9c1f43051cbdceff6080391
Oleg <shevchenko.oleg.87@gmail.com>
2022-02-06 17:23:57 +0300

    first comit

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import style from './Message.module.css'
+
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div className={style.content}>
+            <div className={style.avatar}>
+                <img src={props.avatar} alt="avatar"/>
+            </div>
+            <div className={style.message}>
+                <div className={style.name}>{props.name}</div>
+                <div className={style.messageText}>{props.message}</div>
+                <div className={style.tmstmp}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
