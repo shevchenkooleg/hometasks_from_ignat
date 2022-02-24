fd63e968af1c5e46423b05ccd0850a55b47fbba3
Oleg <shevchenko.oleg.87@gmail.com>
2022-02-24 23:31:55 +0300

    propcessing hw04

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -39,7 +39,7 @@ function HW4() {
 
                 {/*----------------------------------------------------*/}
 
-                <SuperButton>
+                <SuperButton className={s.qqq}>
                     default
                 </SuperButton>
 
