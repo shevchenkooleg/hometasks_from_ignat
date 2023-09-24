6f362eba6fbf0b0e2c29ac90ca53d0bff4211fdc
Oleg <shevchenko.oleg.87@gmail.com>
2023-09-24 09:44:13 +0300

    hw11

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -5,6 +5,7 @@ import HW2 from "../../../p2-homeworks/h2/HW2";
 import HW3 from '../../../p2-homeworks/h3/HW3';
 import HW4 from '../../../p2-homeworks/h4/HW4';
 import HW5 from '../../../p2-homeworks/h5-rrd-v6/HW5';
+import HW11 from '../../../p2-homeworks/h11/HW11';
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
             {/*<HW2/>*/}
             {/*<HW3/>*/}
             {/*<HW4/>*/}
-            <HW5/>
+            <HW11/>
 
         </div>
     )
